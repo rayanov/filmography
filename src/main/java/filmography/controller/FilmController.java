14ace9e198df659ea4d13d456434593812010a72
rayanov <ramil-rayanov@mail.ru>
2021-02-02 17:37:35 +0500

    Initial commit

---
+++ src/main/java/filmography/controller/FilmController.java
@@ -0,0 +1,35 @@
+package filmography.controller;
+
+import filmography.model.Film;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FilmController {
+    private static Film film;
+
+    static {
+        film = new Film();
+        film.setTitle("Inception");
+        film.setYear(2010);
+        film.setGenre("sci-fi");
+        film.setWatched(true);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView allFilms() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("films");
+        modelAndView.addObject("film", film);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.GET)
+    public ModelAndView editPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("editPage");
+        return modelAndView;
+    }
+}
---
+++ src/main/webapp/WEB-INF/pages/films.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ramil
+  Date: 02.02.2021
+  Time: 12:19
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    Hello World
+    <h2><a href="/edit>">${film}</a></h2>
+</body>
+</html>
